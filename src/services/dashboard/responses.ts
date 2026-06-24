bda0a59fcbcf8061713702642b62a6f2fe383a2f
Taslim Yusuf <Omotoyosi2210@gmail.com>
2026-06-24 14:58:33 +0100

    About to start integrating  /response

---
+++ src/services/dashboard/responses.ts
@@ -0,0 +1,14 @@
+import api from "../axios"
+
+export async function getGlobalResponse(params: {
+    page?: number,
+    limit?: number,
+}) {
+    try {
+        const response = await api.get('api/v1/responses', {params})
+        return response.data
+    } catch (error) {
+        console.error("get global response error:", error)
+        throw error
+    }
+}
